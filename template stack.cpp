b5e5bda95ca8988994a673bc0b7d000ac3b7a3a0
Einsla <tsppck@gmail.com>
2025-01-10 00:50:06 +0800

    Update and rename template.cpp to template stack.cpp

---
+++ template stack.cpp
@@ -0,0 +1,66 @@
+#include <iostream>
+#include <cassert>
+using namespace std;
+
+template<typename Type>
+struct node {
+	Type data;
+	node<Type>* link;
+};
+
+template<typename Type>
+class stack {
+	private:
+		node<Type>* top, * current;
+		int max, count;
+
+	public:
+		stack() {
+			top = current = nullptr;
+			max = 50;
+			count = 0;
+		}
+		~stack() {
+			destroy();
+		}
+
+		Type Top() {
+			return top->data;
+		}
+		void Push(Type item) {
+			node<Type>* newNode;
+			newNode = new node<Type>;
+			assert(newNode != nullptr);
+			newNode->data = item;
+			newNode->link = nullptr;
+
+			if (top == nullptr) {
+				top = newNode;
+			}
+			else {
+				newNode->link = top;
+				top = newNode;
+			}
+			count++;
+		}
+		void Pop() {
+			current = top;
+			if (current != nullptr) {
+				top = current->link;
+				delete current;
+				count--;
+			}
+		}
+		bool Full() {
+			return (count == max);
+		}
+		void destroy() {
+			current = top;
+			while (current != nullptr) {
+				top = current->link;
+				delete current;
+				current = top;
+			}
+			count = 0;
+		}
+};
