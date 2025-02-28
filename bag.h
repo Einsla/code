e159d5553700b4f4f87ce46b9d537dceed84e083
Einsla <tsppck@gmail.com>
2025-02-28 13:25:01 +0800

    Add files via upload

---
+++ bag.h
@@ -0,0 +1,180 @@
+#pragma once
+#include <iostream>
+#include "item.h"
+using namespace std;
+
+struct space {
+	item key;
+	space* next;
+	space* previous;
+};
+
+class bag {
+private:
+	space* first, * last;
+	int count, max = 500;
+
+public:
+	bag() {
+		first = last = nullptr;
+		count = 0;
+	}
+	~bag() {
+		destroy();
+	}
+
+	int getcount() const {
+		return count;
+	}
+	void click(int n) {
+		if (n > count) {
+			return;
+		}
+		else if (n > 0) {
+			space* temp;
+			temp = first;
+			n--;
+			while (n > 0) {
+				temp = temp->next;
+				n--;
+			}
+			temp->key.display();
+			if (temp->key.getquantity() == 0) {
+				if (temp == first) {
+					first = temp->next;
+				}
+				else {
+					temp->previous->next = temp->next;
+				}
+				if (temp == last) {
+					last = temp->previous;
+				}
+				else {
+					temp->next->previous = temp->previous;
+				}
+				count--;
+			}
+		}
+	}
+	void destroy() {
+		space* temp;
+		while (first != nullptr) {
+			temp = first;
+			first = temp->next;
+			delete temp;
+			first->previous = nullptr;
+			count--;
+		}
+	}
+	void insert(item& Item) {
+		space* temp, * add;
+		add = new space;
+		add->key = Item;
+		add->next = add->previous = nullptr;
+
+		if (count == max) {
+			cout << "bag is full. Please clear up some space\n";
+			delete add;
+			return;
+		}
+		if (first == nullptr) {
+			first = last = add;
+		}
+		else {
+			temp = first;
+			while (temp != nullptr) {
+				if (Item < temp->key) {
+					add->previous = temp->previous;
+					add->next = temp;
+					temp->previous = add;
+					if (temp == first) {
+						first = add;
+					}
+					else {
+						add->previous->next = add;
+					}
+					break;
+				}
+				else if (Item == temp->key) {
+					temp->key.add(Item);
+					delete add;
+					return;
+				}
+				else {
+					if (temp->next != nullptr) {
+						temp = temp->next;
+					}
+					else {
+						temp->next = add;
+						add->previous = temp;
+						last = add;
+						break;
+					}
+				}
+			}
+		}
+		count++;
+	}
+	void display() {
+		space* temp;
+		int n = 0;
+		temp = first;
+		while (temp != nullptr) {
+			n++;
+			cout << n << ". " << temp->key.getname() << endl;
+			temp = temp->next;
+		}
+		cout << endl << endl << endl;
+		cout << n + 1 << ". back to main." << endl;
+
+	}
+};
+
+int main() {
+	char choice;
+	int n;
+	bag world;
+	item ref;
+	do {
+		system("cls");
+		cout << "========================================\n";
+		cout << "	Welcome to my item world	\n";
+		cout << "========================================\n";
+		cout << endl;
+		cout << "Available action:\n";
+		cout << "1.pick up item\n";			//insert item
+		cout << "2.open bag\n";				//print item list
+		cout << "3.exit game\n";
+		cout << "\n\n\nSo what will you do?\n";
+		cin >> choice;
+		switch (choice) {
+		case'1':
+			system("cls");
+			create(ref);
+			world.insert(ref);
+			system("pause");
+			break;
+		case'2':
+			while (true) {
+				system("cls");
+				world.display();
+				cout << endl << endl;
+				cin >> n;
+				if (n == (world.getcount() + 1)) {
+					cout << "Returning to menu..." << endl;
+					system("pause");
+					break;
+				}
+				world.click(n);
+			};
+			break;
+		case'3':
+			exit(0);
+			break;
+		default:
+			cout << "invalid choice\n";
+			cout << "Please choose again!!!\n";
+			break;
+		}
+	} while (choice != '3');
+}
---
+++ item.h
@@ -0,0 +1,214 @@
+﻿#pragma once
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct effect {
+	int round;
+	string Eff;
+	effect* link;
+
+	effect() {
+		round = 0;
+		Eff = " ";
+		link = nullptr;
+	}
+	~effect() {
+		round = 0;
+		Eff = " ";
+		link = nullptr;
+	}
+
+	void apply() const {
+		system("cls");
+		if(Eff == "burn"){
+			cout << "+------------------------------------------------+\n";
+			cout << "| cause burn on target for " << round << " rounds              |\n";
+			cout << "| target continuously loss hp...                 |\n";
+			cout << "+------------------------------------------------+\n";
+		}
+		else if (Eff == "frozen") {
+			cout << "+------------------------------------------------+\n";
+			cout << "| cause frost on target for " << round << " rounds             |\n";
+			cout << "| target can't move...                           |\n";
+			cout << "+------------------------------------------------+\n";
+		}
+		else if (Eff == "electrocute") {
+			cout << "+------------------------------------------------+\n";
+			cout << "| cause electric on target for " << round << " rounds          |\n";
+			cout << "| target loss 50 agility                         |\n";
+			cout << "+------------------------------------------------+\n";
+		}
+		else if (Eff == "heal") {
+			cout << "+------------------------------------------------+\n";
+			cout << "| hp recover by 50 for " << round << " rounds                   |\n";
+			cout << "| continuously recovering...                      |\n";
+			cout << "+------------------------------------------------+\n";
+		}
+	}
+};
+
+class chain {
+private:
+	effect* first, * last;
+
+public:
+	chain() {
+		first = last = nullptr;
+	}
+	~chain() {
+		destroy();
+	}
+
+	void add_effect(string e, int r) {
+		effect* add;
+		add = new effect;
+		add->Eff = e;
+		add->round = r;
+		add->link = nullptr;
+
+		if (first == nullptr) {
+			first = last = add;
+		}
+		else {
+			last->link = add;
+			last = add;
+		}
+	}
+	void apply() {
+		effect* temp;
+		temp = first;
+		while (temp != nullptr) {
+			temp->apply();
+			temp = temp->link;
+		}
+	}
+	void destroy() {
+		effect* temp;
+		temp = first;
+		while (temp != nullptr) {
+			first = temp->link;
+			delete temp;
+			temp = first;
+		}
+		first = last = nullptr;
+	}
+
+	chain& operator=(const chain& other) {
+		if (this != &other) {
+			destroy();
+			effect* temp = other.first;
+			while (temp) {
+				add_effect(temp->Eff, temp->round);
+				temp = temp->link;
+			}
+		}
+		return *this;
+	}
+};
+
+class item {
+private:
+	int code,quantity;
+	string name, description;
+	chain status;
+
+public:
+	//c&d
+	item() {
+		code = quantity = 0;
+		name = description = " ";
+	}
+	~item() {
+		code = quantity = 0;
+		name = description = " ";
+	}
+
+	void set(int c, int n, string na, string de, chain &s) {
+		code = c;
+		quantity = n;
+		name = na;
+		description = de;
+		status = s;
+	}
+	string getname() {
+		return name;
+	}
+	int getquantity() const {
+		return quantity;
+	}
+	void use(int n) {
+		quantity -= n;
+		while (n > 0) {
+			status.apply();
+			n--;
+		}
+	}
+	void display() {
+		int n = 0;
+		char c;
+		do {
+			system("cls");
+			cout << name << endl;
+			cout << description << endl;
+			cout << quantity << endl;
+			cout << "would you like to use this item?\n";
+			cin >> c;
+			if (c == 'Y') {
+				cout << "How many item to be use?\n";
+				cin >> n;
+				if (n > quantity) {
+					cout << "insufficient item\n";
+				}
+				else {
+					use(n);
+					return;
+				}
+			}
+			else if (c == 'N') {
+				return;
+			}
+			else {
+				cout << "invalid command\n";
+			}
+			system("pause");
+		} while ((c != 'N' and c != 'Y') or n > quantity);
+		
+
+	}
+	void add(item &i) {
+		quantity += i.quantity;
+	}
+
+	bool operator <(item& target) {
+		return (code < target.code);
+	}
+	bool operator ==(item& target) {
+		return code == target.code;
+	}
+
+	friend void create(item& a) {
+		int c, q, r;
+		string n, d, e;
+		chain s;
+		cout << "name        : ";
+		getline(cin >> ws, n);
+		cout << "description : ";
+		getline(cin >> ws, d);
+		cout << "code        : ";
+		cin >> c;
+		cout << "quantity    : ";
+		cin >> q;
+		while(true){
+			cout << "effect      : ";
+			getline(cin >> ws, e);
+			if (e == "none") {
+				break;
+			}
+			cout << "round       : ";
+			cin >> r;
+			s.add_effect(e,r);
+		}
+		a.set(c,q,n,d,s);
+	}
+};
